4173c17bc67e1398a5a0d7ef01df3b51b7d3c44b
Ahmad Fatayri <ahmadfatayri@Houssams-MacBook-Pro.local>
2019-04-02 10:55:06 +0300

    Init proj

---
+++ versi-app/versi-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  versi-app
+//
+//  Created by Ahmad Fatayri on 4/2/19.
+//  Copyright © 2019 Ahmad Fatayri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
